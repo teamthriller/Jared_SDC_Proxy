7eb90ff8ff7ec0df198556be7dfcab64697b7c8f
DmitriTarasov <dmta9796@colorado.edu>
2019-04-10 20:29:54 -0600

    inital commit of proxy server (serves up my service so far)

---
+++ index.js
@@ -0,0 +1,21 @@
+const cors = require('cors');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3111;
+app.use(cors());
+app.use(express.static(path.join(__dirname, '/public')));
+app.listen(port, () => console.log(`App listening to port ${port}`));
+
+app.get('/app.js', (req,res) => {
+    
+
+});
+// launch all 4 services.
+// serve html.
+// get the apps through scripts. 
+// 
+//
+//
+//
