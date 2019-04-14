b6295cbc06eb7164db4942989b082f746f771577
DmitriTarasov <dmta9796@colorado.edu>
2019-04-13 23:40:53 -0600

    proxy file

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
 const cors = require('cors');
 const express = require('express');
 const path = require('path');
+const httpproxy = require('http-proxy');
 
 const app = express();
 const port = 3111;
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const proxy = httpproxy.createProxyServer({
+    target: "http://ec2-52-91-196-238.compute-1.amazonaws.com/"
+});
+
+app.all("/data/toptracks", cors(), (req, res) => {
+  proxy.web(req, res, {
+    target: "http://ec2-18-191-178-115.us-east-2.compute.amazonaws.com/"
+  });
+});
+
+app.get('/data/albumswithartist/*', (req, res) => {
+    proxy.web(req, res, {
+      target: 'http://ec2-13-58-138-231.us-east-2.compute.amazonaws.com/'
+    });
+  });
+  app.get('/data/albumsbyartist/*', (req, res) => {
+    proxy.web(req, res, {
+      target: 'http://ec2-13-58-138-231.us-east-2.compute.amazonaws.com/'
+    });
+  });
+  app.get('/data/epswithartist/*', (req, res) => {
+    proxy.web(req, res, {
+      target: 'http://ec2-13-58-138-231.us-east-2.compute.amazonaws.com/'
+    });
+  });
+  app.get('/data/compilationswithartist/*', (req, res) => {
+    proxy.web(req, res, {
+      target: 'http://ec2-13-58-138-231.us-east-2.compute.amazonaws.com/'
+    });
+  });
+
+app.all("/data/artist/", (req, res) => {
+  proxy.web(req, res, {
+    target: "http://ec2-52-91-196-238.compute-1.amazonaws.com/"
+  });
+});
+
+app.all("/icon", (req, res) => {
+    proxy.web(req, res, {
+      target: "http://ec2-52-91-196-238.compute-1.amazonaws.com/"
+    });
+  });
+
 app.listen(port, () => console.log(`App listening to port ${port}`));
-// launch all 4 services.
-// serve html.
-// get the apps through scripts. 
-// 
-//
-//
-//
