c5fae73242a0590dbb104847999cb793c2c68551
Jared Brink <J-radical@Jareds-MacBook-Pro.local>
2019-04-26 14:22:15 -0600

    service running on proxy server

--- index.js
+++ index.js
@@ -1,60 +1,58 @@
-const cors = require('cors');
-const express = require('express');
-const path = require('path');
-const httpproxy = require('http-proxy');
+const cors = require("cors");
+const express = require("express");
+const path = require("path");
+const httpproxy = require("http-proxy");
 
 const app = express();
-const port = 3111;
+const port = process.env.PORT || 3111;
+
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+const proxy = httpproxy.createProxyServer({});
 
-const proxy = httpproxy.createProxyServer({
-    target: "http://ec2-34-227-148-64.compute-1.amazonaws.com"
-});
-
-app.all("/data/toptracks", cors(), (req, res) => {
+app.all("/artists/*", cors(), (req, res) => {
   proxy.web(req, res, {
-    target: "http://ec2-18-191-178-115.us-east-2.compute.amazonaws.com/"
+    target: "http://localhost:3100"
   });
 });
 
-app.get('/data/albumswithartist/*', (req, res) => {
-    proxy.web(req, res, {
-      target: 'http://ec2-13-58-138-231.us-east-2.compute.amazonaws.com/'
-    });
-  });
-  app.get('/data/albumsbyartist/*', (req, res) => {
-    proxy.web(req, res, {
-      target: 'http://ec2-13-58-138-231.us-east-2.compute.amazonaws.com/'
-    });
-  });
-  app.get('/data/epswithartist/*', (req, res) => {
-    proxy.web(req, res, {
-      target: 'http://ec2-13-58-138-231.us-east-2.compute.amazonaws.com/'
-    });
-  });
-  app.get('/data/compilationswithartist/*', (req, res) => {
-    proxy.web(req, res, {
-      target: 'http://ec2-13-58-138-231.us-east-2.compute.amazonaws.com/'
-    });
-  });
+// app.get("/data/albumswithartist/*", (req, res) => {
+//   proxy.web(req, res, {
+//     target: "http://ec2-13-58-138-231.us-east-2.compute.amazonaws.com/"
+//   });
+// });
+// app.get("/data/albumsbyartist/*", (req, res) => {
+//   proxy.web(req, res, {
+//     target: "http://ec2-13-58-138-231.us-east-2.compute.amazonaws.com/"
+//   });
+// });
+// app.get("/data/epswithartist/*", (req, res) => {
+//   proxy.web(req, res, {
+//     target: "http://ec2-13-58-138-231.us-east-2.compute.amazonaws.com/"
+//   });
+// });
+// app.get("/data/compilationswithartist/*", (req, res) => {
+//   proxy.web(req, res, {
+//     target: "http://ec2-13-58-138-231.us-east-2.compute.amazonaws.com/"
+//   });
+// });
 
-app.all("/data/artist/", (req, res) => {
-  proxy.web(req, res, {
-    target: "http://ec2-34-227-148-64.compute-1.amazonaws.com"
-  });
-});
+// app.all("/data/artist/", (req, res) => {
+//   proxy.web(req, res, {
+//     target: "http://ec2-34-227-148-64.compute-1.amazonaws.com"
+//   });
+// });
 
-app.all("/icon", (req, res) => {
-    proxy.web(req, res, {
-      target: "http://ec2-34-227-148-64.compute-1.amazonaws.com"
-    });
-  });
-app.all("/data/artist/*", (req, res) => {
-  proxy.web(req, res, {
-    target: "http://ec2-18-191-230-44.us-east-2.compute.amazonaws.com/"
-  });
-});
+// app.all("/icon", (req, res) => {
+//   proxy.web(req, res, {
+//     target: "http://ec2-34-227-148-64.compute-1.amazonaws.com"
+//   });
+// });
+// app.all("/data/artist/*", (req, res) => {
+//   proxy.web(req, res, {
+//     target: "http://ec2-18-191-230-44.us-east-2.compute.amazonaws.com/"
+//   });
+// });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
